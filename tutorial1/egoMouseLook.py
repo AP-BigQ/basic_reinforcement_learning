48ec5b46b8b59c38b0b072b691d0af348dbe0c9c
Alan <hao.shopaholic@gmail.com>
2025-04-30 12:35:54 -0400

    Add venv and pygame in vscode --tutorial1 is now working in GUI

--- tutorial1/egoMouseLook.py
+++ tutorial1/egoMouseLook.py
@@ -5,6 +5,8 @@ import shelve
 
 import pdb
 
+import os
+
 import cellular
 # reload(cellular)  # Python 2
 importlib.reload(cellular)
@@ -132,7 +134,10 @@ mouse = Mouse()
 cat = Cat()
 cheese = Cheese()
 
-world = cellular.World(Cell, directions=directions, filename='../worlds/waco.txt')
+##
+print(f"Attempting to open file: {os.path.abspath('./worlds/waco.txt')}")
+
+world = cellular.World(Cell, directions=directions, filename='./worlds/waco.txt')
 world.age = 0
 
 world.addAgent(cheese, cell=pickRandomLocation())
